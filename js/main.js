260ff7210227bed5be1226a65e46e075055560b4
ktauchert <61756592+ktauchert@users.noreply.github.com>
2021-09-01 09:33:38 +0200

    changed whoe profile page

---
+++ js/main.js
@@ -0,0 +1,217 @@
+const about = document.querySelector('#about')
+const contact = document.querySelector('#contact')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+
+const ahp = document.querySelector('#ahp');
+const ahpContent = document.querySelector('#ahp-content');
+const asik = document.querySelector('#asik');
+const asikContent = document.querySelector('#asik-content');
+const manuthetic = document.querySelector('#manuthetic');
+const manutheticContent = document.querySelector('#manuthetic-content');
+const openDarkness = document.querySelector('#open-darkness');
+const openDarknessContent = document.querySelector('#open-darkness-content');
+const diverse = document.querySelector('#diverse');
+const diverseContent = document.querySelector('#diverse-content');
+
+const impressum = document.querySelector('#impressum');
+const impressumContent = document.querySelector('#impressum-content');
+
+const colorFocusOne = '#00aa00';
+const colorFocusTwo = '#4444f8';
+const colorBlur = '#777';
+
+let windowWidth = '400px';
+let windowHeight = '400px';
+let windowTop = 50;
+let windowRight = 50;
+let windowBottom = 50;
+let windowLeft = 50;
+
+
+about.addEventListener('click', () => {
+    const aboutBox = new WinBox({
+        title: 'Über mich',
+        width: windowWidth,
+        height: windowHeight,
+        top: windowTop,
+        right: windowRight,
+        bottom: windowBottom,
+        left: windowLeft,
+        mount: aboutContent,
+        onfocus: function (){
+            this.setBackground(colorFocusOne);
+        },
+        onblur: function (){
+            this.setBackground(colorBlur);
+        },
+        
+    })
+})
+contact.addEventListener('click', () => {
+    const contactBox = new WinBox({
+        title: 'Kontaktiere mich',
+        width: windowWidth,
+        height: windowHeight,
+        top: windowTop,
+        right: windowRight,
+        bottom: windowBottom,
+        left: windowLeft,
+        mount: contactContent,
+        onfocus: function (){
+            this.setBackground(colorFocusOne);
+        },
+        onblur: function (){
+            this.setBackground(colorBlur);
+        },
+    })
+})
+
+ahp.addEventListener('click', () => {
+    const ahpBox = new WinBox({
+        title: 'AHP-Plattform',
+        width: windowWidth,
+        height: windowHeight,
+        top: windowTop,
+        right: windowRight,
+        bottom: windowBottom,
+        left: windowLeft,
+        mount: ahpContent,
+        onfocus: function (){
+            this.setBackground(colorFocusTwo);
+        },
+        onblur: function (){
+            this.setBackground(colorBlur);
+        },
+    })
+})
+asik.addEventListener('click', () => {
+    const asikBox = new WinBox({
+        title: 'ASIK-Tool',
+        width: windowWidth,
+        height: windowHeight,
+        top: windowTop,
+        right: windowRight,
+        bottom: windowBottom,
+        left: windowLeft,
+        mount: asikContent,
+        onfocus: function (){
+            this.setBackground(colorFocusTwo);
+        },
+        onblur: function (){
+            this.setBackground(colorBlur);
+        },
+    })
+})
+manuthetic.addEventListener('click', () => {
+    const manutheticBox = new WinBox({
+        title: 'Manuthetic',
+        width: windowWidth,
+        height: windowHeight,
+        top: windowTop,
+        right: windowRight,
+        bottom: windowBottom,
+        left: windowLeft,
+        mount: manutheticContent,
+        onfocus: function (){
+            this.setBackground(colorFocusTwo);
+        },
+        onblur: function (){
+            this.setBackground(colorBlur);
+        },
+    })
+})
+openDarkness.addEventListener('click', () => {
+    const openDarknessBox = new WinBox({
+        title: 'OpenDarkness',
+        background: '#00aa00',
+        width: windowWidth,
+        height: windowHeight,
+        top: windowTop,
+        right: windowRight,
+        bottom: windowBottom,
+        left: windowLeft,
+        mount: openDarknessContent,
+        onfocus: function (){
+            this.setBackground(colorFocusTwo);
+        },
+        onblur: function (){
+            this.setBackground(colorBlur);
+        },
+    })
+})
+diverse.addEventListener('click', () => {
+    const contentBox = new WinBox({
+        title: 'Diverses',
+        background: '#00aa00',
+        width: windowWidth,
+        height: windowHeight,
+        top: windowTop,
+        right: windowRight,
+        bottom: windowBottom,
+        left: windowLeft,
+        mount: diverseContent,
+        onfocus: function (){
+            this.setBackground(colorFocusTwo);
+        },
+        onblur: function (){
+            this.setBackground(colorBlur);
+        },
+    })
+})
+impressum.addEventListener('click', () => {
+    const contentBox = new WinBox({
+        title: 'Impressum',
+        background: '#00aa00',
+        width: '95%',
+        height: '95%',
+        top: windowTop,
+        right: windowRight,
+        bottom: windowBottom,
+        left: windowLeft,
+        mount: impressumContent,
+        onfocus: function (){
+            this.setBackground(colorFocusTwo);
+        },
+        onblur: function (){
+            this.setBackground(colorBlur);
+        },
+    })
+})
+
+function screenAdjust(){
+    // XS
+    if (window.matchMedia('screen and (max-width: 600px)').matches) {
+        console.log("media 1");
+        windowTop = windowRight = windowBottom = windowLeft = 10;
+        windowWidth = '320px';
+        windowHeight = '460px';
+    }
+    if (window.matchMedia('screen and (min-width: 600px)').matches) {
+        console.log("media 2");
+        windowTop = windowRight = windowBottom = windowLeft = 15;
+        windowWidth = '400px';
+        windowHeight = '500px';
+    }
+    if (window.matchMedia('screen and (min-width: 992px)').matches) {
+        console.log("media 3");
+        windowTop = windowRight = windowBottom = windowLeft = 30;
+        windowWidth = '520px';
+        windowHeight = '600px';
+    }
+    if (window.matchMedia('screen and (min-width: 1200px)').matches) {
+        console.log("media 4");
+        windowTop = windowRight = windowBottom = windowLeft = 60;
+        windowWidth = '600px';
+        windowHeight = '700px';
+    }
+}
+
+window.addEventListener('resize', screenAdjust);
+// DOM READY
+document.addEventListener('DOMContentLoaded', () => {
+    screenAdjust();
+}, false);
+
+
+
--- main.js
+++ main.js
@@ -1,91 +1,111 @@
-import './style.css'
+import "./css/style.css";
+import "./css/cat.css";
+
+// import "./js/winbox.bundle.js"
+// import "./js/main.js"
 
 // Option 1: Import the entire three.js core library.
-import * as THREE from 'three';
-import * as dat from 'dat.gui';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three";
+import * as dat from "dat.gui";
+import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const gui = new dat.GUI();
+const INITIAL_COLOR = {
+  r: 0.1,
+  g: 0.1,
+  b: 0.1,
+};
+const HOVER_COLOR = {
+  r: 0.3,
+  g: 0.3,
+  b: 0.3,
+};
+let frame = 0;
 const world = {
   plane: {
-    width: 10,
-    height: 10,
-    widthSegments: 10,
-    heightSegments: 10
-  }
-}
-gui.add(world.plane, 'width', 1, 20).onChange(generatePlane)
-gui.add(world.plane, 'height', 1, 20).onChange(generatePlane)
-gui.add(world.plane, 'widthSegments', 1, 20).onChange(generatePlane)
-gui.add(world.plane, 'heightSegments', 1, 20).onChange(generatePlane)
+    width: 200,
+    height: 200,
+    widthSegments: 50,
+    heightSegments: 50,
+  },
+};
 
+const raycaster = new THREE.Raycaster();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
   innerWidth / innerHeight,
-  0.1, 1000
-)
-const renderer = new THREE.WebGLRenderer()
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(
-  innerWidth, innerHeight
-)
-// no jagged 
+renderer.setSize(innerWidth, innerHeight);
+// no jagged
 renderer.setPixelRatio(devicePixelRatio);
-document.body.appendChild(
-  renderer.domElement
-)
-new OrbitControls(
-  camera, renderer.domElement
-)
-camera.position.z = 5;
+document.body.appendChild(renderer.domElement);
+// new OrbitControls(camera, renderer.domElement);
+camera.position.z = 30;
 
-// // A BOX IN GREEN
-// const boxGeometry = new THREE.BoxGeometry(
-//   1, 1, 1
-// )
-// const material = new THREE.MeshBasicMaterial(
-//   { color: 0x00FF00 }
-// )
-// const mesh = new THREE.Mesh(
-//   boxGeometry, material
-// )
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+//  INIT Plane
 
-//  A Plane
 const planeGeometry = new THREE.PlaneGeometry(
-  10, 10, 10, 10
-)
-const planeMaterial = new THREE.MeshPhongMaterial(
-  {
-    color: 0xFF0000,
-    side: THREE.DoubleSide,
-    flatShading: THREE.FlatShading
-  }
-)
-const planeMesh = new THREE.Mesh(
-  planeGeometry, planeMaterial
-)
+  world.plane.width,
+  world.plane.height,
+  world.plane.widthSegments,
+  world.plane.heightSegments
+);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  // color: 0xFF0000,
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
+  vertexColors: true,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
 
-console.log(planeMesh.geometry.attributes.position.array);
-randomizeZVertices();
+planeMesh.geometry.attributes.position.originalPosition = planeMesh.geometry.attributes.position.array;
+// Init randomize Vertices
+randomizeVertices();
+
+// Initial Color
+const colors = [];
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+  colors.push(INITIAL_COLOR.r, INITIAL_COLOR.g, INITIAL_COLOR.b);
+}
+planeMesh.geometry.setAttribute(
+  "color",
+  new THREE.BufferAttribute(new Float32Array(colors), 3)
+);
+
 // Lighting
-const light = new THREE.DirectionalLight(
-  0xFFFFFF, 1
-)
-light.position.set(0, 0, 1)
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
 scene.add(light);
 
-function randomizeZVertices() {
+
+function randomizeVertices() {
+  let randomValues = [];
   const { array } = planeMesh.geometry.attributes.position;
 
-  for (let i = 0; i < array.length; i += 3) {
-    const x = array[i];
-    const y = array[i + 1];
-    const z = array[i + 2];
+  for (let i = 0; i < array.length; i++) {
 
-    array[i + 2] = z + Math.random()
+    if (i % 3 === 0) {
+      const x = array[i];
+      const y = array[i + 1];
+      const z = array[i + 2];
+
+      array[i] = x + (Math.random() - 0.5) * 2;
+      array[i + 1] = y + (Math.random() - 0.5) * 3;
+      array[i + 2] = z + (Math.random() - 0.5) * 3;
+    }
+    randomValues.push(Math.random() * Math.PI * 2);
   }
+  planeMesh.geometry.attributes.position.randomValues = randomValues;
 }
 function generatePlane() {
   planeMesh.geometry.dispose();
@@ -95,11 +115,103 @@ function generatePlane() {
     world.plane.widthSegments,
     world.plane.heightSegments
   );
-  randomizeZVertices();
+  randomizeVertices();
+  // mouseover color
+  const colors = [];
+  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(INITIAL_COLOR.r, INITIAL_COLOR.g, INITIAL_COLOR.b);
+  }
+  planeMesh.geometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  );
+  planeMesh.geometry.attributes.position.originalPosition = planeMesh.geometry.attributes.position.array;
 }
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   // planeMesh.rotation.x += 0.01;
+  raycaster.setFromCamera(mouse, camera);
+  frame += 0.01;
+
+  const { array, originalPosition, randomValues } = planeMesh.geometry.attributes.position;
+
+  for (let i = 0; i < array.length; i += 3) {
+    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01;
+    array[i + 1] = originalPosition[i + 1] + Math.sin(frame + randomValues[i]) * 0.01;
+
+  }
+  planeMesh.geometry.attributes.position.needsUpdate = true;
+
+  const intersects = raycaster.intersectObject(planeMesh);
+  if (intersects.length > 0) {
+    const { color } = intersects[0].object.geometry.attributes;
+
+    // RGB Red
+    color.setX(intersects[0].face.a, HOVER_COLOR.r);
+    color.setX(intersects[0].face.b, HOVER_COLOR.r);
+    color.setX(intersects[0].face.c, HOVER_COLOR.r);
+
+    // RGB Green
+    color.setY(intersects[0].face.a, HOVER_COLOR.g);
+    color.setY(intersects[0].face.b, HOVER_COLOR.g);
+    color.setY(intersects[0].face.c, HOVER_COLOR.g);
+
+    // RGB Blue
+    color.setZ(intersects[0].face.a, HOVER_COLOR.b);
+    color.setZ(intersects[0].face.b, HOVER_COLOR.b);
+    color.setZ(intersects[0].face.c, HOVER_COLOR.b);
+
+    color.needsUpdate = true;
+
+    setTimeout(() => {
+      color.setX(intersects[0].face.a, INITIAL_COLOR.r);
+      color.setX(intersects[0].face.b, INITIAL_COLOR.r);
+      color.setX(intersects[0].face.c, INITIAL_COLOR.r);
+
+      // RGB Green
+      color.setY(intersects[0].face.a, INITIAL_COLOR.g);
+      color.setY(intersects[0].face.b, INITIAL_COLOR.g);
+      color.setY(intersects[0].face.c, INITIAL_COLOR.g);
+
+      // RGB Blue
+      color.setZ(intersects[0].face.a, INITIAL_COLOR.b);
+      color.setZ(intersects[0].face.b, INITIAL_COLOR.b);
+      color.setZ(intersects[0].face.c, INITIAL_COLOR.b);
+      color.needsUpdate = true;
+    }, 500);
+    // gsap.to(HOVER_COLOR, {
+    //   r: INITIAL_COLOR.r,
+    //   g: INITIAL_COLOR.g,
+    //   b: INITIAL_COLOR.b,
+    //   duration: 1,
+    //   onStart: () => {
+    //     //console.log("Update Hover COlor")
+    //     // RGB Red
+    //     color.setX(intersects[0].face.a, HOVER_COLOR.r);
+    //     color.setX(intersects[0].face.b, HOVER_COLOR.r);
+    //     color.setX(intersects[0].face.c, HOVER_COLOR.r);
+
+    //     // RGB Green
+    //     color.setY(intersects[0].face.a, HOVER_COLOR.g);
+    //     color.setY(intersects[0].face.b, HOVER_COLOR.g);
+    //     color.setY(intersects[0].face.c, HOVER_COLOR.g);
+
+    //     // RGB Blue
+    //     color.setZ(intersects[0].face.a, HOVER_COLOR.b);
+    //     color.setZ(intersects[0].face.b, HOVER_COLOR.b);
+    //     color.setZ(intersects[0].face.c, HOVER_COLOR.b);
+    //     color.needsUpdate = true;
+    //   },
+    // });
+  }
 }
 animate();
+
+// eVENT lISTENER
+addEventListener("mousemove", (event) => {
+  // normalized Coords for three js
+  mouse.x = (event.clientX / innerWidth) * 2 - 1;
+  mouse.y = (event.clientY / innerHeight) * -2 + 1;
+});
